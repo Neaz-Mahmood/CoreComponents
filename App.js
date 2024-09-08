ce72e70b88c67f4f9c2568fc7319642e2efc5ad9
Neaz Mahmood <neazn694@gmail.com>
2024-09-08 23:48:26 +0600

    learning presseble

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import { Button, Pressable, Text, View } from 'react-native'
 
-export default function App() { 
+export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 60,
+      }}
+    >
+      <Button title='Press me' onPress={() => alert('Button pressed!')} />
+      <Pressable
+        onPress={() => console.log('Presed')}
+        onLongPress={() => console.log('Long pressed')}
+        onPressIn={() => console.log('Press in')}
+        onPressOut={() => console.log('Press out')}
+      >
+        <Text>
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio.
+          Donec nec odio. Donec nec odio. Donec nec odio. Donec nec odio. Donec
+          nec odio. Donec nec odio. Donec nec odio. Donec nec odio. Donec nec
+          odio.
+        </Text>
+      </Pressable>
     </View>
-  );
+  )
 }
