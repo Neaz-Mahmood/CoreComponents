cb40efeba33a03c571dca6deaa150e08c9654a82
neaz <neaz.mahmood@vivasoftltd.com>
2024-09-22 23:09:27 +0600

    learning layout

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { Button, Modal, Pressable, StatusBar, Text, View } from 'react-native'
+import { Button, Text, View, ActivityIndicator, StyleSheet } from 'react-native'
 import { useState } from 'react'
+import Greet from './components/Greet'
+import Box from './components/Box'
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false)
   return (
     <View
-      style={{
-        flex: 1,
-        backgroundColor: "plum",
-        padding: 60,
-      }}
+      style={styles.container}
     >
-      <Text>
-        Test
-      </Text>
-    <StatusBar backgroundColor='midnightblue' barStyle='light-content' hidden />
+      <Box style={{ backgroundColor: "red"}} >Box 1</Box> 
+      <Box style={{ backgroundColor: "blue"}} >Box 2</Box> 
+      <Box style={{ backgroundColor: "red"}} >Box 3</Box> 
+      <Box style={{ backgroundColor: "blue"}} >Box 4</Box> 
+      <Box style={{ backgroundColor: "red"}} >Box 5</Box> 
+      <Box style={{ backgroundColor: "blue"}} >Box 6</Box> 
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    borderWidth: 6,
+    borderColor: 'black',
+  },
+})
