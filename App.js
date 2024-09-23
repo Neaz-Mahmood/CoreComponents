dbb707c542d0a526c659c7f956d1bcd33db1f5e7
Neaz Mahmood <neazn694@gmail.com>
2024-09-23 08:36:49 +0600

    Learning flex

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
-import { Button, Text, View, ActivityIndicator, StyleSheet } from 'react-native'
+import { Button, Text, View, StyleSheet } from 'react-native'
 import { useState } from 'react'
-import Greet from './components/Greet'
 import Box from './components/Box'
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false)
   return (
-    <View
-      style={styles.container}
-    >
-      <Box style={{ backgroundColor: "red"}} >Box 1</Box> 
-      <Box style={{ backgroundColor: "blue"}} >Box 2</Box> 
-      <Box style={{ backgroundColor: "red"}} >Box 3</Box> 
-      <Box style={{ backgroundColor: "blue"}} >Box 4</Box> 
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: 'red', paddingVertical: 100 }}>Box 1</Box>
+      <Box style={{ backgroundColor: 'blue' }}>Box 2</Box>
+      <Box style={{ backgroundColor: 'red' }}>Box 3</Box>
+      {/* <Box style={{ backgroundColor: "blue"}} >Box 4</Box> 
       <Box style={{ backgroundColor: "red"}} >Box 5</Box> 
-      <Box style={{ backgroundColor: "blue"}} >Box 6</Box> 
-
+      <Box style={{ backgroundColor: "blue"}} >Box 6</Box>  */}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'baseline',
     marginTop: 50,
     borderWidth: 6,
     borderColor: 'black',
